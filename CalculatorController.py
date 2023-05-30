0ec02e2959d3c52028ab991da5589af1cc818a94
Xavier <chabolaxavi@gmail.com>
2023-05-30 15:26:45 +0200

    init

---
+++ CalculatorController.py
@@ -0,0 +1,4 @@
+class CalculatorController():
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
---
+++ CalculatorModel.py
@@ -0,0 +1,17 @@
+class CalculatorModel():
+    def __init__(self, controller):
+        self.controller = controller
+
+    def sum(n1: int, n2: int):
+        return n1+n2
+    
+    def substract(n1: int, n2: int):
+        return n1-n2
+    
+    def multiply(n1: int, n2: int):
+        return n1*n2
+    
+    def divide(n1: int, n2: int):
+        return n1/n2
+    
+                
